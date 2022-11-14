a5efc0ec720778495c55e437158d80a49c9037b3
Nuraiym <nabdirasulkyzy@gmail.com>
2022-11-14 14:25:21 +0500

    hw-18

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import useFetch from "./hooks/useFetch";
+import './App.css'
+function App() {
+  const [todo, setTodo] = useState("");
+  const BASE_URL ="https://homework-18-e3c2d-default-rtdb.firebaseio.com/todo18.json";
+
+  const { todos, addItem, getTodo,removeItemHandler } = useFetch([], BASE_URL);
+
+  const addTodoHendler = (e) => {
+    e.preventDefault();
+    addItem({ text: todo });
+    setTodo("");
+  };
+  useEffect(() => {
+    getTodo();
+  }, []);
+  const demoveItemHandler =(todoId)=>{
+      removeItemHandler(todoId)
+      getTodo(todoId)
+  }
+  return (
+    <div>
+      <form onSubmit={addTodoHendler}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button>add</button>
+      </form>
+      <div>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}
+          <button onClick={() => demoveItemHandler(item.id)}>X</button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
